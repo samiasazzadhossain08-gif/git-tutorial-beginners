77c398f1d2fc60c7690ceb10ea4fc13a42fe40c4
samiasazzadhossain08-gif <samiasazzadhossain08@gmail.com>
2026-07-22 10:18:37 +0300

    Design Completed

--- main.py
+++ main.py
@@ -2,5 +2,8 @@
 import pandas as pd
 import numpy as np
 import os 
+import sklearn
 print("Hello World")
 print("This is a python file hello world program.")
+print("Thank you for using this program.")
+print("End of the python file hello world program.")
